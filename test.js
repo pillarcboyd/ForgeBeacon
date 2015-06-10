d89800e319652d36ace07dd4adce856df4d43b1a
Jason Halbur <jhalbur@Pillar-Mac3s-MacBook-Pro.local>
2015-06-10 14:09:47 -0400

    Initial Site and Test

---
+++ test.js
@@ -0,0 +1,18 @@
+
+
+var webdriver = require('selenium-webdriver'),
+    By = require('selenium-webdriver').By,
+    until = require('selenium-webdriver').until;
+
+var driver = new webdriver.Builder()
+    .forBrowser('firefox')
+    .build();
+
+driver.get('http://localhost:1337');
+driver.wait(function() {
+ return driver.getTitle().then(function(title) {
+   return title === 'Hello World';
+ });
+}, 6000);
+
+driver.quit();
