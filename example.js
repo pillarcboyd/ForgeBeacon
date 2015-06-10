76f95fa1e6253468b610fcc8a0e236e675b5f22f
Zach VanMeter <zvanmeter@honeycrisp.local>
2015-06-10 14:45:52 -0400

    Updated title to Pillar Technology

--- example.js
+++ example.js
@@ -1,6 +1,13 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('<html><head><title>Hello World</title></head><body>My cool html</body></html>\n');
+  res.end('
+          <html>
+          <head>
+          <title>Hello World</title>
+          </head>
+          <body>My cool html</body>
+          </html>
+          ');
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
