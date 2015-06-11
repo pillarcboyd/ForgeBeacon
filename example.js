8cf3e457ccf308b2d24b882c86dc50c241a4c721
Zach VanMeter <zvanmeter@github.com>
2015-06-11 11:52:37 -0400

    Added message to indicate what server node is running on.

--- example.js
+++ example.js
@@ -1,3 +1,6 @@
+var port = '8080'
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8080);
+connect().use(serveStatic(__dirname)).listen(port);
+console.log('Serving up %s on http://localhost:%s', __dirname, port);
+
