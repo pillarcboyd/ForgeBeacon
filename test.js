5a5e4e8cc157e7c44cfa6584257e857b8c07092a
aschacht <wschacht@pillartechnology.com>
2015-06-11 10:23:14 -0400

    dded second test to test the body content

--- test.js
+++ test.js
@@ -1,6 +1,9 @@
 var assert = require('assert'),
 test = require('selenium-webdriver/testing'),
-webdriver = require('selenium-webdriver');
+webdriver = require('selenium-webdriver'),
+By = require('selenium-webdriver').By;
+
+
 
 test.describe('Home Page', function() {
   test.it('should work', function() {
@@ -8,17 +11,31 @@ test.describe('Home Page', function() {
     var driver = new webdriver.Builder().forBrowser('firefox').
     build();
 
-    driver.get('http://localhost:1337');
-
+    driver.get('http://localhost:1337/helloworld.htm');
 
     driver.getTitle().then(function(title) {
-      assert.equal(title,'Hello World Sweet Kicks');
+      assert.equal(title,'Pillar Technology');
     });
+    driver.quit();
+
+  });
+
+  test.it('Body test', function() {
+    this.timeout(15000);
+    var driver = new webdriver.Builder().forBrowser('firefox').
+    build();
+
+    driver.get('http://localhost:1337/helloworld.htm');
 
 
-
+    driver.findElement(By.tagName("body")).getText().then(function(bodyText){
+      assert.equal(bodyText,'Hello World!');
+    });
 
     driver.quit();
 
   });
+
+
+
 });
