8cafd393b2e4a61fe4ad6e95e6f4dbb0b0a97d3f
Zach VanMeter <zvanmeter@github.com>
2015-06-11 09:37:31 -0400

    Updated node to serve stand alone html file

--- example.js
+++ example.js
@@ -1,13 +1,3 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('
-          <html>
-          <head>
-          <title>Hello World</title>
-          </head>
-          <body>My cool html</body>
-          </html>
-          ');
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+var connect = require('connect');
+var serveStatic = require('serve-static');
+connect().use(serveStatic(__dirname)).listen(1337);
