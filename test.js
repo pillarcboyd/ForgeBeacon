64d58c5ee68131795546aabbecdd0716eb2b2f83
rucker <rick.ucker@gmail.com>
2015-06-12 11:45:39 -0400

    Remove redundant var declaration.

--- test.js
+++ test.js
@@ -5,16 +5,14 @@ var webAddress = "http://localhost:8080/",
 assert = require('assert'),
 test = require('selenium-webdriver/testing'),
 webdriver = require('selenium-webdriver'),
-By = require('selenium-webdriver').By,
-driver = new webdriver.Builder().forBrowser('firefox').build();
+By = require('selenium-webdriver').By;
 
 
 
 test.describe('Home Page', function() {
   test.it('Should serve correct page', function() {
     this.timeout(15000);
-    var driver = new webdriver.Builder().forBrowser('firefox').
-    build();
+    var driver = new webdriver.Builder().forBrowser('firefox').build();
 
     driver.get(webAddress);
 
@@ -28,7 +26,7 @@ test.describe('Home Page', function() {
 
   test.it('Should display Check In button', function() {
       this.timeout(15000);
-
+      var driver = new webdriver.Builder().forBrowser('firefox').build();
       driver.get(webAddress);
 
       driver.findElement(By.name("checkInBtn")).getAttribute("value").then(function(btnText){
