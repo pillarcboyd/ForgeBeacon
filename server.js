e8836d8eb2efea4118e7cd4554e592b1a7ac02fd
Zach VanMeter <zvanmeter@github.com>
2015-06-12 11:17:39 -0400

    Removed unneeded index handling code.

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 var port = '8080';
+var servedFiles = __dirname;
 var express = require('express');
 var app = express();
 var http = require('http');
 var httpServer = http.Server(app);
 
-app.use(express.static(__dirname));
+app.use(express.static(servedFiles));
 
-app.get('/', function(req, res){
-      res.sendFile(__dirname + '/index.html');
-});
 app.listen(port);
-console.log('Serving up %s on http://localhost:%s', __dirname, port);
+console.log('Serving up %s on http://localhost:%s', servedFiles, port);
 console.log('Press ctrl + c to cancel.');
