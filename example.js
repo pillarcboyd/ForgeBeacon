e3d45fb67a1e7ef9ab2e438ba5d62672146cd740
Zach VanMeter <zvanmeter@github.com>
2015-06-12 11:00:31 -0400

    Refactored code to remove unneeded commented code.

--- example.js
+++ example.js
@@ -1,16 +1,3 @@
-//var port = '8080'
-//var connect = require('connect');
-//var serveStatic = require('serve-static');
-//var url = require('url');
-//var req = require('request');
-//connect().use(serveStatic(__dirname, {'index': ['index.html', 'index.htm']})).listen(port);
-//console.log('Serving up %s on http://localhost:%s', __dirname, port);
-//var requrl = url.parse(req.url, true);
-//console.log(requrl);
-//var serve = serveStatic('public/ftp', {'index': ['index.html', 'index.htm']})
-//if (requrl.pathname === '/') {
-//  console.log('sup baby?');}
-
 var port = '8080';
 var express = require('express');
 var app = express();
