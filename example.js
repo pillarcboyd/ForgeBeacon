57c89ddbb8a20cdc5010462f10b0e0c36b25bcdb
Zach VanMeter <zvanmeter@github.com>
2015-06-12 10:55:31 -0400

    Updated to allow serving up a index.htm file.  Express Library now required.

--- example.js
+++ example.js
@@ -1,6 +1,27 @@
-var port = '8080'
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(port);
-console.log('Serving up %s on http://localhost:%s', __dirname, port);
+//var port = '8080'
+//var connect = require('connect');
+//var serveStatic = require('serve-static');
+//var url = require('url');
+//var req = require('request');
+//connect().use(serveStatic(__dirname, {'index': ['index.html', 'index.htm']})).listen(port);
+//console.log('Serving up %s on http://localhost:%s', __dirname, port);
+//var requrl = url.parse(req.url, true);
+//console.log(requrl);
+//var serve = serveStatic('public/ftp', {'index': ['index.html', 'index.htm']})
+//if (requrl.pathname === '/') {
+//  console.log('sup baby?');}
 
+var port = '8080';
+var express = require('express');
+var app = express();
+var http = require('http');
+var httpServer = http.Server(app);
+
+app.use(express.static(__dirname));
+
+app.get('/', function(req, res){
+      res.sendFile(__dirname + '/index.htm');
+});
+app.listen(port);
+console.log('Serving up %s on http://localhost:%s', __dirname, port);
+console.log('Press ctrl + c to cancel.');
