ab1bc33ae93183cf8679f8090e43c913e4de2659
Casey Boyd <caseyboyd@Nicholass-MacBook-Pro.local>
2015-06-16 13:39:33 -0400

    Making email get call relative path

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     var from,to,subject,text;
     $("#checkInBtn").click(function(){
       document.getElementById('topMsg').innerHTML="Thank you! Please have a seat. We'll be right with you!";
-      $.get("http://localhost:8080/send",function(data){
+      $.get("/send",function(data){
           if(data=="sent")
           {
 
